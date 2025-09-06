9faffe8174dfbcf708c82caca6fefd1c32210a53
balajidande1 <balajidande7@gmail.com>
2025-09-06 16:23:29 +0530

    rm

--- banking-frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8765', 
-  customerServiceUrl: 'http://localhost:8081/api/customers'
-  // Your API Gateway or microservices base URL
-};
